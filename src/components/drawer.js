4df6c8ab46426c8859b6988082740fb40b150942
vladplesu <vladp1987@gmail.com>
2019-02-09 09:38:32 +0200

    first commit

---
+++ src/components/drawer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
---
+++ src/components/layout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+function Layout(props) {
+  const { classes } = props;
+  const { children } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        {children}
+      </Grid>
+    </div>
+  );
+}
+
+Layout.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Layout);
