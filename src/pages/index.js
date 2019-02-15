f2790e36ec21d3b5481bd1dbf22f504de280ab0f
vladplesu <vladp1987@gmail.com>
2019-02-15 22:47:04 +0200

    created and added custom theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import Layout from '../components/layout';
 import MyAppBar from '../components/appBar';
 import Hero from '../components/hero';
 import AboutMe from '../components/about';
 
 // TODO: Create custom theme using createMuiTheme method
+const theme = createMuiTheme(
+  {
+    palette: {
+      primary: { main: '#263238', contrastText: '#7ce3ff' },
+      secondary: { main: '#7ce3ff', contrastText: '#000000' },
+    },
+  },
+  'Outer Space Anakiwa Guinea',
+);
 
 export default () => (
-  <Layout>
-    {/* TODO: Add custom theme using MuiThemeProvider component */}
-    {/* TODO: Move MyAppBar component to Layout component */}
-    <MyAppBar />
-    <Hero />
-    {/* TODO: Add AboutMe component */}
-    <AboutMe />
-    {/* TODO: Add Experience component */}
-    {/* TODO: Add Skills component */}
-    {/* TODO: Add Education component */}
-    {/* TODO: Add Portfolio component */}
-    {/* TODO: Add Contact Component */}
-  </Layout>
+  <MuiThemeProvider theme={theme}>
+    <Layout>
+      {/* TODO: Add custom theme using MuiThemeProvider component */}
+      {/* TODO: Move MyAppBar component to Layout component */}
+      <MyAppBar />
+      <Hero />
+      {/* TODO: Add AboutMe component */}
+      <AboutMe />
+      {/* TODO: Add Experience component */}
+      {/* TODO: Add Skills component */}
+      {/* TODO: Add Education component */}
+      {/* TODO: Add Portfolio component */}
+      {/* TODO: Add Contact Component */}
+    </Layout>
+  </MuiThemeProvider>
 );
