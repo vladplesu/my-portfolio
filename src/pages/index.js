c1441e751e5dfb281624282be7446fcebe60d6b6
vladplesu <vladp1987@gmail.com>
2019-02-27 23:49:21 +0200

    moved theme to separate file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import Layout from '../components/layout';
 import Hero from '../components/hero';
 import AboutMe from '../components/about';
 import MyExp from '../components/experience';
-
-// Create custom theme using createMuiTheme method
-const theme = createMuiTheme(
-  {
-    palette: {
-      primary: { main: '#263238', contrastText: '#7ce3ff' },
-      secondary: { main: '#7ce3ff', contrastText: '#000000' },
-      text: { primary: '#fff' },
-      divider: '#7ce3ff',
-    },
-  },
-  'Outer Space Anakiwa Guinea',
-);
+import theme from '../theme/theme';
 
 export default () => (
   <MuiThemeProvider theme={theme}>
---
+++ src/theme/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme(
+  {
+    palette: {
+      primary: { main: '#263238', contrastText: '#7ce3ff' },
+      secondary: { main: '#7ce3ff', contrastText: '#000000' },
+      text: { primary: '#fff' },
+      divider: '#7ce3ff',
+    },
+  },
+  'Outer Space Anakiwa Guinea',
+);
