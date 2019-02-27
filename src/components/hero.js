871241801c229ae752d3da803efe704f4062c664
vladplesu <vladp1987@gmail.com>
2019-02-27 23:49:58 +0200

    added styled section to hero container

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,19 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import styled from 'styled-components';
 import heroImg from '../images/hero_img.png';
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    height: '75vh',
-    background: `url(${heroImg})`,
-    backgroundSize: 'cover',
-    display: 'flex',
-    alignItems: 'flex-end',
-    justifyContent: 'flex-end',
-    flexDirection: 'column',
-  },
   title: {
     color: 'white',
     marginRight: theme.spacing.unit * 2,
@@ -25,11 +16,21 @@ const styles = theme => ({
   },
 });
 
+const HeroSection = styled.section`
+  align-items: flex-end;
+  background: url(${heroImg});
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  height: 75vh;
+`;
+
 function Hero(props) {
   const { classes } = props;
 
   return (
-    <div className={classes.root} color="primary">
+    <HeroSection>
       <Typography className={classes.title} variant="h4" color="primary">
         Vlad Plesu
       </Typography>
@@ -42,7 +43,7 @@ function Hero(props) {
       >
         Front-End Developer
       </Typography>
-    </div>
+    </HeroSection>
   );
 }
 
