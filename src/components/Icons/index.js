35de8d4534cad592ea2beb9488056697e623b997
vladplesu <vladp1987@gmail.com>
2019-03-05 00:02:49 +0200

    modified icons component and skills container

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import blueGrey from '@material-ui/core/colors/blueGrey';
 import iconList from './icon-list';
 
+const styles = theme => ({
+  icon: {
+    margin: theme.spacing.unit,
+    fontSize: 80,
+  },
+});
+
 function Icons(props) {
-  const { icon } = props;
+  const {
+    classes, icon, stopOffsetTop, stopOffsetBottom, colorFill,
+  } = props;
   return (
-    <SvgIcon {...props}>
+    <SvgIcon
+      className={classes.icon}
+      component={svgProps => (
+        <svg {...svgProps}>
+          <defs>
+            <linearGradient id={`gradient-${icon}`} gradientTransform="rotate(90)">
+              <stop offset="1" stopColor={blueGrey[800]}>
+                <animate
+                  attributeName="offset"
+                  from="1"
+                  to={stopOffsetTop}
+                  dur="2.5s"
+                  fill="freeze"
+                />
+              </stop>
+              <stop offset="1" stopColor={colorFill}>
+                <animate
+                  attributeName="offset"
+                  from="1"
+                  to={stopOffsetBottom}
+                  dur="2.5s"
+                  fill="freeze"
+                />
+              </stop>
+            </linearGradient>
+          </defs>
+          {React.cloneElement(svgProps.children[0], {
+            fill: `url(#gradient-${icon})`,
+          })}
+        </svg>
+      )}
+      {...props}
+    >
       <path d={iconList[icon]} />
     </SvgIcon>
   );
@@ -14,10 +57,17 @@ function Icons(props) {
 
 Icons.defaultProps = {
   icon: '',
+  stopOffsetTop: '0%',
+  stopOffsetBottom: '0%',
+  colorFill: '#fff',
 };
 
 Icons.propTypes = {
+  classes: PropTypes.object.isRequired,
   icon: PropTypes.string,
+  stopOffsetTop: PropTypes.string,
+  stopOffsetBottom: PropTypes.string,
+  colorFill: PropTypes.string,
 };
 
-export default Icons;
+export default withStyles(styles)(Icons);
--- src/containers/Skills/index.js
+++ src/containers/Skills/index.js
@@ -1,71 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, Typography } from '@material-ui/core';
-import blueGrey from '@material-ui/core/colors/blueGrey';
+import { Typography, Grid, withStyles } from '@material-ui/core';
 
 import Section from '../../components/section';
 import Icons from '../../components/Icons';
 
-// TODO: Add custom styles variables
-const styles = theme => ({
-  icon: {
-    margin: theme.spacing.unit,
-    fontSize: 80,
+const style = {
+  gridItem: {
+    textAlign: 'center',
   },
-});
+};
 
 // TODO: Add function Skills
 function Skills(props) {
   const { classes } = props;
-
   return (
     <Section id="skills">
       <Typography variant="headline" color="secondary">
         My Skills
       </Typography>
-      <Icons
-        className={classes.icon}
-        icon="html5"
-        component={svgProps => (
-          <svg {...svgProps}>
-            <defs>
-              <linearGradient id="gradient1" gradientTransform="rotate(90)">
-                <stop offset="50%" stopColor={blueGrey[800]} />
-                <stop offset="70%" stopColor="#e44d26" />
-              </linearGradient>
-            </defs>
-            {React.cloneElement(svgProps.children[0], {
-              fill: 'url(#gradient1)',
-            })}
-          </svg>
-        )}
-      />
-      <Icons
-        className={classes.icon}
-        icon="css3"
-        component={svgProps => (
-          <svg {...svgProps}>
-            <defs>
-              <linearGradient id="gradient2" gradientTransform="rotate(90)">
-                <stop offset="20%" stopColor={blueGrey[800]} />
-                <stop offset="40%" stopColor="#264de4" />
-              </linearGradient>
-            </defs>
-            {React.cloneElement(svgProps.children[0], {
-              fill: 'url(#gradient2)',
-            })}
-          </svg>
-        )}
-      />
-      <Icons icon="js" />
+      <Grid container justify="center">
+        <Grid item xs={12} sm className={classes.gridItem}>
+          <Icons icon="html5" stopOffsetTop="0.1" stopOffsetBottom="0.5" colorFill="#e44d26" />
+          <Typography variant="h6">HTML</Typography>
+        </Grid>
+        <Grid item xs={12} sm className={classes.gridItem}>
+          <Icons icon="css3" stopOffsetTop="0.4" stopOffsetBottom="0.6" colorFill="#0073B1" />
+          <Typography variant="h6">CSS</Typography>
+        </Grid>
+        <Grid item xs={12} sm className={classes.gridItem}>
+          <Icons icon="js" stopOffsetTop="0.6" stopOffsetBottom="0.8" colorFill="#f7df1e" />
+          <Typography variant="h6">Javascript</Typography>
+        </Grid>
+      </Grid>
     </Section>
   );
 }
 
-// TODO: Add propTypes
-Skills.propTypes = {
+Skills.prototype = {
   classes: PropTypes.object.isRequired,
 };
 
 // TODO: Export component
-export default withStyles(styles)(Skills);
+export default withStyles(style)(Skills);
